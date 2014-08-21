436f59054377c5826c532b49be6e27e1e7e1a8de
Anibal Valadez <ivaladez@anzen.com.mx>
2014-08-21 18:16:53 -0500

    Estilos, estructura nueva y servicios

--- www/js/main.js
+++ www/js/main.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
-	//inicializar panel
-	$( "body>[data-role='panel']" ).panel();
-
+	//JSON
 	var url="http://www.siervaspadreyermo.org/app/ws/ws.php";
 	$.getJSON(url,function(data){
 		//noticias portada
 		var noticiasPortada = data[0].noticias;
 		for(i = 0; i < noticiasPortada.length; i++){
-			console.log(noticiasPortada[i].titulo)
+			//console.log(noticiasPortada[i].titulo)
 			var newLi =
 			"<li>"
 			+"<a href='#noticia' data-link='"+i+"'>"
@@ -28,17 +26,21 @@ $(document).ready(function() {
 			
 			var newItem =
 			"<h1>"+noticiasPortada[noticiaDetalle].titulo+"</h1>"
-			+"<span>"+noticiasPortada[noticiaDetalle].dia_fecha+noticiasPortada[noticiaDetalle].mes_fecha+noticiasPortada[noticiaDetalle].anio_fecha+"</span>"
+			+"<span>"+noticiasPortada[noticiaDetalle].dia_fecha+"<span>"+noticiasPortada[noticiaDetalle].mes_fecha+noticiasPortada[noticiaDetalle].anio_fecha+"</span>"+"</span>"
 			+"<div class='img-not'><img src='http://www.siervaspadreyermo.org/app/"+noticiasPortada[noticiaDetalle].foto1+"'></div>"
 			+"<div class='res-not'>"+noticiasPortada[noticiaDetalle].resumen+"</div>";
 
-			$('#leer_noticia').empty()
+			var newVideo =
+			"<iframe width='100%' height='300px' src='http://www.youtube.com/embed/"+noticiasPortada[noticiaDetalle].video+"?rel=0 frameborder='0' allowfullscreen></iframe>";
 
-			$(newItem).appendTo("#leer_noticia");				
-		});
-		
-		$('.ico-video').click(function(){
-			$(this).append('<iframe width="100%" height="100%" src="//www.youtube.com/embed/'+noticiasPortada[noticiaDetalle].video+'?rel=0" frameborder="0" allowfullscreen></iframe>')
+			var newAudio =
+			"<iframe width='100%' height='300px' src='http://www.youtube.com/embed/"+noticiasPortada[noticiaDetalle].audio+"?rel=0 frameborder='0' allowfullscreen></iframe>";
+
+			$('#leer_noticia, #videoplayer, #audioplayer').empty()
+
+			$(newItem).appendTo("#leer_noticia");
+			$(newVideo).appendTo("#videoplayer");
+			$(newAudio).appendTo("#audioplayer");				
 		});
 
 		var colorPortada = Math.floor((Math.random() * 10) + 1);
@@ -53,83 +55,28 @@ $(document).ready(function() {
 			$('h4:first').css('background','#d77f1e');
 			$('#listado_portada li:first .datos-lista > span').css('background','#fdab50');
 		}
-
-		//panel
-		$.each(data[1].sedes, function(i,sede){
-			var newSede =
-			"<li><a href='panel_noticias.html'>"+sede.sede+"</a></li>";
-
-			$(newSede).appendTo('#panel_noticias');
-		});
-
-		//tema
-		$.each(data[2].temas, function(i,tema){
-			var newTema =
-			"<li><a href='panel_temas.html'>"+tema.tema+"</a></li>";
-
-			$(newTema).appendTo('#panel_articulos');
-		});
-
 	});
 
 	//footer
-	$('div[data-role="footer"] a').click(function(){
+	$('#portada div[data-role="footer"] a').click(function(){
 		$('div[data-role="footer"] a').removeClass('active');
 		$(this).addClass('active');
 	});
 
-	//panel
-	$('#myPanel h2').click(function(){
-		var textoH2 = $(this).text();
-
-		if(textoH2 === 'Noticias'){
-			$('#myPanel ul').height(0);
-			$('#myPanel i').css('transform','rotate(0)');
-			$('#myPanel i').css('-ms-transform','rotate(0)');
-			$('#myPanel i').css('-webkit-transform','rotate(0)');
-
-			$(this).children().css('transform','rotate(90deg)');
-			$(this).children().css('-ms-transform','rotate(90deg)');
-			$(this).children().css('-webkit-transform','rotate(90deg)');
-			$('#panel_noticias').height('auto');
-		}
-		if(textoH2 === 'Artículos'){
-			$('#myPanel ul').height(0);
-			$('#myPanel i').css('transform','rotate(0)');
-			$('#myPanel i').css('-ms-transform','rotate(0)');
-			$('#myPanel i').css('-webkit-transform','rotate(0)');
-			
-			$(this).children().css('transform','rotate(90deg)');
-			$(this).children().css('-ms-transform','rotate(90deg)');
-			$(this).children().css('-webkit-transform','rotate(90deg)');
-			$('#panel_articulos').height('auto');
-		}
-		if(textoH2 === 'Información'){
-			$('#myPanel ul').height(0);
-			$('#myPanel i').css('transform','rotate(0)');
-			$('#myPanel i').css('-ms-transform','rotate(0)');
-			$('#myPanel i').css('-webkit-transform','rotate(0)');
-			
-			$(this).children().css('transform','rotate(90deg)');
-			$(this).children().css('-ms-transform','rotate(90deg)');
-			$(this).children().css('-webkit-transform','rotate(90deg)');
-			$('#panel_info').height('auto');
-		}
-	});
-
 	//FLICKR
-	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-		{
-			tags: "padre yermo",
-			tagmode: "any",
-			format: "json"
-		}, function(data){
-			$.each(data.items, function(i,item){
-				$("<img/>").attr("src", item.media.m).appendTo(".galeria");
-				if ( i == 999 ) return false;
-			});
+	var apiKey = 'c2d64c71e95ae71604e95ae775b7cc02';
+	var photosetId = '72157633915274589';
+	var fotosUrl = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key="+apiKey+"&photoset_id="+photosetId+"&format=json&jsoncallback=?";
+	$.getJSON(fotosUrl,function(data){
+		$.each(data.photoset.photo, function(i, flickrPhoto){
+	        var basePhotoURL = 'http://farm' + flickrPhoto.farm + '.static.flickr.com/'
+	        + flickrPhoto.server + '/' + flickrPhoto.id + '_' + flickrPhoto.secret + ".jpg";            
+
+	        var a_href = "http://www.flickr.com/photos/" + data.photoset.owner + "/" + flickrPhoto.id + "/";
+	        $("<img/>").attr("src", basePhotoURL).appendTo(".galeria").wrap(("<div class='galeria_item'></div>"))
+		});
 	});
-	
+
 });
 
 $(document).on("pagecreate",function(event){
